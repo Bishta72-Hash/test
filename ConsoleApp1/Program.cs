cdaee2ad44d922b7d00eccca44c6615fb22bd1f2
Anil Bisht <abisht@ghx.com>
2024-07-02 15:03:09 +0530

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading.Tasks;
+using CCXCommon;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        DateTime currentDate = DateTime.Now;
+        Console.WriteLine("Local Time: " + currentDate);
+        // Specify the desired time zone (Mountain Standard Time)
+        string timeZoneId = "Mountain Standard Time";
+        TimeZoneInfo mountainZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+        // Convert the local time to the desired time zone
+        DateTime mountainTime = TimeZoneInfo.ConvertTime(currentDate, mountainZone);
+        Console.WriteLine("Mountain Time: " + mountainTime);
+    }
+
+    private static void testMethod()
+    {
+        string server = @"CCX-SQL-STG-02.WINDSI.GHX.COM\EXPERT01";
+        string database = "ItemStagingCenter";
+        string userID = "ccnet_app";
+        string password = "trubadelo";
+
+        string ConnectionString = @"Server={0};Database={1};uid={2};pwd={3}; Max Pool Size = 150;TrustServerCertificate=True;Encrypt=True;";
+        ConnectionString = String.Format(ConnectionString, server, database, userID, password);
+
+        using (var connection = new SqlConnection(ConnectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO PriceSyncReport_IncomingFileQueue (InputFileName, InputFileS3URL, ProviderEID, StatusID, CreatedOn) VALUES (@InputFileName, @InputFileS3URL,@ProviderEID, @StatusID, @CreatedOn)";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@InputFileName", "text");
+                command.Parameters.AddWithValue("@InputFileS3URL", "text");
+                command.Parameters.AddWithValue("@ProviderEID", "121");
+                command.Parameters.AddWithValue("@StatusID", 1);
+                command.Parameters.AddWithValue("@CreatedOn", DateTime.Now);
+
+                int rowsAffected = command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TestClass
+    {
+        private int _t;
+        public int testInt
+        {
+            get
+            {
+                return _t;
+            }
+            set
+            {
+                _t = value;
+            }
+        }
+        public int a;
+
+        [JobParameter]
+        public int sst { get; set; }
+
+        [JobParameter]
+        public int te;
+    }
+
+    public class JobParameterAttribute : Attribute
+    {
+
+    }
+}
